2eb75f9d2ccbd07014fd7c390603a1b03c517d39
Julio Julao <33884758+juliojulao@users.noreply.github.com>
2022-01-19 16:28:46 -0800

    Update models.py

--- opgg/models.py
+++ opgg/models.py
@@ -24,33 +24,33 @@ class Summoner(models.Model):
         for i in last_match:
             match_detail = watcher.match.by_id("americas", i)
             participants = []
-            for row in match_detail['metadata']['participants']:
+            for row in match_detail['info']['participants']:
                 participants_row = {}
-                participants_row['ign'] = match_detail["participantIdentities"][int(row['participantId']-1)]['player']['summonerName']
+                participants_row['ign'] = row['summonerName']
                 participants_row['region'] = self.region
-                participants_row['champion'] = getChampion(champ_dict, row['championId'])
+                participants_row['champion'] = row['championName']
                 participants_row['ss1'] = ssName(summs_dict, row['spell1Id'])
                 participants_row['ss2'] = ssName(summs_dict, row['spell2Id'])
-                if row['stats']['win'] == True:
+                if row['win'] == True:
                     participants_row['Win'] = 'Win'
                 else:
                     participants_row['Win'] = 'Loss'
-                participants_row['kills'] = row['stats']['kills']
-                participants_row['deaths'] = row['stats']['deaths']
-                participants_row['assists'] = row['stats']['assists']
+                participants_row['kills'] = row['kills']
+                participants_row['deaths'] = row['deaths']
+                participants_row['assists'] = row['assists']
                 participants_row['kda'] = f"{participants_row['kills']}/{participants_row['deaths']}/{participants_row['assists']}"
-                participants_row['totaldamageDealt'] = format(row['stats']['totalDamageDealt'], ',d')
-                participants_row['gold'] = format(row['stats']['goldEarned'], ',d')
-                participants_row['champLevel'] = row['stats']['champLevel']
-                participants_row['creepScore'] = row['stats']['totalMinionsKilled']
+                participants_row['totaldamageDealt'] = format(row['totalDamageDealt'], ',d')
+                participants_row['gold'] = format(row['goldEarned'], ',d')
+                participants_row['champLevel'] = row['champLevel']
+                participants_row['creepScore'] = row['totalMinionsKilled']
                 participants_row['items'] = [
-                    getItem(items_dict, row['stats']['item0']),
-                    getItem(items_dict, row['stats']['item1']),
-                    getItem(items_dict, row['stats']['item2']),
-                    getItem(items_dict, row['stats']['item3']),
-                    getItem(items_dict, row['stats']['item4']),
-                    getItem(items_dict, row['stats']['item5']),
-                    getItem(items_dict, row['stats']['item6'])
+                    getItem(items_dict, row['item0']),
+                    getItem(items_dict, row['item1']),
+                    getItem(items_dict, row['item2']),
+                    getItem(items_dict, row['item3']),
+                    getItem(items_dict, row['item4']),
+                    getItem(items_dict, row['item5']),
+                    getItem(items_dict, row['item6'])
                 ]
                 participants.append(participants_row)
 
